4f5f8fb905357c9e657d7b18bc64cce224fe21f5
rose <rosedorleans1@gmail.com>
2022-10-27 17:03:52 +0200

    end part one

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>PHP Class and functions</title>
     <style>
         body { margin: 0; }
+        strong { color: red; }
     </style>
 </head>
 <body>
@@ -15,42 +16,57 @@ require_once('./src/part-two.php');
 <ul>
     <h2>Les fonctions en PHP</h2>
     <li>
-        Résultat de l'exercice 1 : <strong><?php  ?></strong>
+        Résultat de l'exercice 1 : <strong><?= helloWorld() ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 2 : (utilisez la valeur 3 en paramètre dans cette exemple): <strong><?php  ?></strong>
+        Résultat de l'exercice 2 
+        (utilisez la valeur 3 en paramètre dans cette exemple) : 
+        <strong><?= multiplyByHundred(3)  ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 3 :(utilisez les arguments Hello world) <strong><?php  ?></strong>
+        Résultat de l'exercice 3
+        (utilisez les arguments Hello world) :
+        <strong><?= concatenate("Hello", "world") ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 4 :(utilisez 2 fois la fonction, une fois avec le paramètre 6 et l'autre avec paramètre 12)
-        <strong><?php  ?></strong>
+        Résultat de l'exercice 4
+        (utilisez 2 fois la fonction, une fois avec le paramètre 6 et l'autre avec paramètre 12) :
+        <strong><?= maximumTen(6) ?></strong>
+        <strong><?= maximumTen(12) ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 5 :(utilisez 2 fois la fonction, une fois avec les paramètres 6 et 12 et l'autre avec paramètre 24 et 13)
-        <strong><?php  ?></strong>
+        Résultat de l'exercice 5 
+        (utilisez 2 fois la fonction, une fois avec les paramètres 6 et 12 et l'autre avec paramètre 24 et 13) :
+        <strong><?= returnSmaller(6, 12) ?></strong>
+        <strong><?= returnSmaller(24, 13) ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 6 :(utilisez le tableau suivant) : <?php $fruitsArray = ['Abricots', 'Pomme', 'Poire', 'Banane'] ;?>
-        <strong><?php  ?></strong>
+        Résultat de l'exercice 6 
+        (utilisez le tableau suivant) : 
+        <?php $fruitsArray = ['Abricots', 'Pomme', 'Poire', 'Banane'] ;?>
+        <strong><?= getFirstElementOfArray($fruitsArray) ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 7 :(utilisez le tableau $fruitsArray également et trouvez l'index de Poire) :
-        <strong><?php  ?></strong>
+        Résultat de l'exercice 7 
+        (utilisez le tableau $fruitsArray également et trouvez l'index de Poire) :
+        <strong><?= getKeyOfString($fruitsArray) ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 8 :(utilisez le tableau $integerValues) : <?php $integerValues = [1, 34, 45, 2, 28,39,56, 12, 100] ;?>
-        <strong><?php  ?></strong>
+        Résultat de l'exercice 8 
+        (utilisez le tableau $integerValues) : 
+        <?php $integerValues = [1, 34, 45, 2, 28, 39, 56, 12, 100] ;?>
+        <strong><?= sumAllArraysValues($integerValues) ?></strong>
     </li>
     <li>
-        Résultat de l'exercice 9 :(utilisez deux fois la fonction, une fois avec le mot 'bonjour' et une fois avec le mot 'glycosylphosphatidylinositol') :
-        <strong><?php  ?></strong>
+        Résultat de l'exercice 9 
+        (utilisez deux fois la fonction, une fois avec le mot 'bonjour' et une fois avec le mot 'glycosylphosphatidylinositol') :
+        <strong><?= isMoreThanTen("bonjour") ?></strong>
+        <strong><?= isMoreThanTen("glycosylphosphatidylinositol") ?></strong>
     </li>
 
     <li>
         Résultat de l'exercice 10 :
-        <strong><?php  ?></strong>
+        <strong><?= getUserAverageScore() ?></strong>
     </li>
 </ul>
 
--- src/part-one.php
+++ src/part-one.php
@@ -23,50 +23,65 @@ function concatenate(string $a, string $b)
 
 # Exercice 4 : la fonction suivante prend en argument un integer, si l'argument
 # est plus grand que 10 la fonction retourne 10 sinon on retourne la valeur
-function maximumTen()
+function maximumTen(int $a)
 {
-
+    $answer = false;
+    if($a > 10){
+        $answer = true;
+    }
+    return $answer;
 }
 
 # Exercice 5 : la fonction suivante prend en arguments 2 integer, renvoyer l'integer le plus petit des arguments
-function returnSmaller()
+function returnSmaller(int $a, int $b)
 {
-
+    $answer = $a;
+    if($a > $b){
+        $answer = $b;
+    }
+    return $answer;
 }
 
 # Exercice 6 : Retournez le premier éléments du tableau passé en argument de fonction
-function getFirstElementOfArray()
+function getFirstElementOfArray(array $array)
 {
-
+    return $array[0];
 }
 
 # Exercice 7 : la fonction suivante prend 2 arguments, le premier est un tableau, le deuxième est une string
 # retournez l'index de la string dans le tableau
-function getKeyOfString()
+function getKeyOfString(array $array)
 {
-
+    return array_search('Poire', $array);
 }
 
 # Exercice 8 : la fonction suivantes prend en argument un tableau contenant uniquement des integers
 # retournez la somme de toutes les valeurs du tableau
-function sumAllArraysValues()
+function sumAllArraysValues(array $array)
 {
-
+    return array_sum($array);
 }
 
 #Exercice 9 : la foncion suivantes prend en argument une string, si elle fait moins de 10 caractères
 # retournez 'Too short' sinon renvoyez l'argument
-function isMoreThanTen(){
-
+function isMoreThanTen(string $a)
+{
+    $answer = $a;
+    if(strlen($a) < 10){
+        $answer = 'Too short';
+    }
+    return $answer;
 }
 
 #Exercice 10 : calculer la moyenne des valeurs présentes dans la fonction
-function getUserAverageScore(){
+function getUserAverageScore()
+{
     $mathScore = 15;
     $englishScore = 12;
     $scienceScore = 9;
 
-    $average = 0; // Remplacez $average par votre calcul
+    $array = array_filter([$mathScore, $englishScore, $scienceScore]);
+    $average = array_sum($array) / count($array);
 
     return 'La moyenne est de '.$average.' / 20.';
 }
